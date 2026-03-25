61888c06087dafc054a1136daf02cf7747adb180
oabnerwilliam <abnerwilliam026@gmail.com>
2026-03-25 18:41:38 -0300

    feat: adicionando fluxo de usuários na página inicial

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,25 @@
-export default function Home() {
+import { prisma } from "@/lib/prisma"
+import { UserForm } from "./components/UserForm"
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card"
+
+export default async function Home() {
+  const users = await prisma.user.findMany()
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="flex flex-col gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {users.map((user) => (
+          <Card key={user.id}>
+            <CardHeader>
+              <CardTitle>{user.name}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p>{user.email}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+      <UserForm />
     </div>
-  );
+  )
 }
